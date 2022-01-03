6d705cff21c605e02d423eeac90369c5432f0bbd
yo76317 <yo76317@gmail.com>
2022-01-03 09:37:20 +0800

    1.送分內容先做 2.頁首和頁尾切出，使用include

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 <html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
 <title>健康促進網</title>
-<link href="./home_files/css.css" rel="stylesheet" type="text/css">
-<script src="./home_files/jquery-1.9.1.min.js"></script>
-<script src="./home_files/js.js"></script>
+<link href="./css/css.css" rel="stylesheet" type="text/css">
+<script src="./js/jquery-1.9.1.min.js"></script>
+<script src="./js/js.js"></script>
 </head>
 
 <body>
@@ -14,35 +14,31 @@
 </div>
 <iframe name="back" style="display:none;"></iframe>
 	<div id="all">
-    	<div id="title">
-        00 月 00 號 Tuesday | 今日瀏覽: 1 | 累積瀏覽: 36        </div>
-        <div id="title2">
-        	
-        </div>
+    	
+		<?php include header.php ?>
+
         <div id="mm">
         	<div class="hal" id="lef">
-            	                	    <a class="blo" href="?do=po">分類網誌</a>
-               	                     	    <a class="blo" href="?do=news">最新文章</a>
-               	                     	    <a class="blo" href="?do=pop">人氣文章</a>
-               	                     	    <a class="blo" href="?do=know">講座訊息</a>
-               	                     	    <a class="blo" href="?do=que">問卷調查</a>
-               	                 </div>
+            	<a class="blo" href="?do=po">分類網誌</a>
+               	<a class="blo" href="?do=news">最新文章</a>
+               	<a class="blo" href="?do=pop">人氣文章</a>
+               	<a class="blo" href="?do=know">講座訊息</a>
+               	<a class="blo" href="?do=que">問卷調查</a>
+            </div>
             <div class="hal" id="main">
             	<div>
-            		
-                	<span style="width:18%; display:inline-block;">
-                    	                    	<a href="?do=login">會員登入</a>
-                    	                    </span>
-                    	<div class="">
-                		                        </div>
-                </div>
-            </div>
-        </div>
-        <div id="bottom">
-    	    本網站建議使用：IE9.0以上版本，1024 x 768 pixels 以上觀賞瀏覽 ， Copyright © 2012健康促進網社群平台 All Right Reserved 
-    		 <br>
-    		 服務信箱：health@test.labor.gov.tw<img src="./home_files/02B02.jpg" width="45">
-        </div>
-    </div>
+					<!-- 改跑馬燈 -->
+            		<marquee style="width:82%; display:inline-block;">請民眾踴躍跑馬燈</marquee><span style="width:18%; display:inline-block;">
+                    	<a href="?do=login">會員登入</a>
+                    </span>
+                	<div class="">
+					</div>
+        		</div>
+    		</div>
+    	</div>
+
+		<?php include footer.php ?>
+
+	</div>
 
 </body></html>
